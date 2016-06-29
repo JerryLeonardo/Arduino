3a08ddcbd3027cdc3134471f656b8fed936e75a0
JerryLeonardo <b10107124@mail.ntust.edu.tw>
2016-06-29 17:16:45 +0800

    Update BasicDrum.ino

--- BasicDrum/BasicDrum.ino
+++ BasicDrum/BasicDrum.ino
@@ -1,9 +1,9 @@
-const int threshold = 200;    // 敲擊蜂鳴片的臨界值
-unsigned long startTime = 0;  // 記錄開始「擊鼓」的時間
-boolean active = 0;           // 代表是否在「作用期間」
-unsigned long offTime = 150;  // 代表「敲擊訊號截止時間」 
-boolean firstHit = 1;         // 代表是否為「初次敲擊訊號」 
-boolean setTime = 0;          // 代表是否「已設定起始時間」
+const int threshold = 200;    // 敲擊蜂鳴片的臨界值 threshold of knocking of the pizeo
+unsigned long startTime = 0;  // 記錄開始「擊鼓」的時間 record the start time of the knock
+boolean active = 0;           // 代表是否在「作用期間」represent whether it's in the "knocking" period
+unsigned long offTime = 150;  // 代表「敲擊訊號截止時間」 represent the ending time of the knocking
+boolean firstHit = 1;         // 代表是否為「初次敲擊訊號」 represet wether it's the first time to knock
+boolean setTime = 0;          // 代表是否「已設定起始時間」represent wether it has the starting time
 
 void midiMsg(byte cmd, byte pitch, byte velocity) {
   Serial.write(cmd);
@@ -19,9 +19,9 @@ void loop() {
   int val = analogRead(A0);
 
   if((val > threshold)) {
-    if (firstHit == 1) {       // 若是「初次敲擊訊號」 
-      if (setTime == 0) {      // 若尚未「設定起始時間」
-        startTime = micros();  // 記錄起始時間值（微秒）
+    if (firstHit == 1) {       // 若是「初次敲擊訊號」 if it's the first time
+      if (setTime == 0) {      // 若尚未「設定起始時間」if there is no starting time
+        startTime = micros();  // 記錄起始時間值（微秒）recoding the starting time
         setTime = 1;
       }
       
